525d958c6988b505e9fc93e095303cfc4397dd5c
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-27 22:58:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
+//console.log(isPalindrome('Anna'))
 
 /* 
   Add your pseudocode here
+  change word to toLowerCase
+loop through the string checking the first and last characters if they match each time removing the last character
+return true or false
 */
+
 
 /*
   Add written explanation of your solution here
@@ -14,12 +27,12 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("AnnA"));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("PeteRR"));
 }
 
 module.exports = isPalindrome;
